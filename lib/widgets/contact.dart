ee0b6069a4d93ac2b7bf70ae1f87336f7b326da4
Jose A. Pinell <japinell@yahoo.com>
2025-09-28 14:43:25 -0700

    Implemented placeholder for ContactWidget feature

---
+++ lib/widgets/contact.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class ContactWidget extends StatelessWidget {
+  const ContactWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Contact: Coming soon...",
+        style: TextStyle(fontSize: 19.0, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
