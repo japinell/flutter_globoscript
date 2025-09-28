8c4948383a7c5696a07f486709cf6d3bbb21e223
Jose A. Pinell <japinell@yahoo.com>
2025-09-28 14:16:46 -0700

    Added script_data json file

---
+++ lib/data/script_data.dart
@@ -0,0 +1,17 @@
+final glyphInfo = [
+  {"glyph": "あ", "info": "Sounds like A in father"},
+  {"glyph": "い", "info": "Sounds like EE in tee"},
+  {"glyph": "う", "info": "Sounds like OO in too"},
+  {
+    "glyph": "え",
+    "info": "Sounds like AY in may, but without rounding the vowel",
+  },
+  {"glyph": "お", "info": "Sounds like O in no, but without rounding the vowel"},
+];
+
+final lessonInfo = [
+  {"code": "L1", "name": "Hiragana"},
+  {"code": "L2", "name": "Katakana"},
+  {"code": "L3", "name": "Kanji"},
+  {"code": "L4", "name": "Putting all together"},
+];
