24e86f6b6124771fac0bd1891aa7a503d241e2e5
Jose A. Pinell <japinell@yahoo.com>
2025-09-28 14:41:03 -0700

    Implemented placeholder for CommunityWidget feature

---
+++ lib/widgets/community.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class CommunityWidget extends StatelessWidget {
+  const CommunityWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Community: Coming soon...",
+        style: TextStyle(fontSize: 19.0, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
