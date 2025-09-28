9365256a6c9c6b41278c426970065a474142ee62
Jose A. Pinell <japinell@yahoo.com>
2025-09-28 14:28:22 -0700

    Implemented GlyphListWidget feature

--- lib/widgets/glyph_list.dart
+++ lib/widgets/glyph_list.dart
@@ -1,10 +1,22 @@
 import "package:flutter/material.dart";
+import "package:flutter_globoscript/data/script_data.dart" show glyphInfo;
 
 class GlyphListWidget extends StatelessWidget {
   const GlyphListWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      children: <Widget>[
+        for (var glyph in glyphInfo)
+          ListTile(
+            leading: Text(
+              glyph["glyph"]!,
+              style: TextStyle(fontSize: 23.0, fontWeight: FontWeight.bold),
+            ),
+            title: Text(glyph["info"]!),
+          ),
+      ],
+    );
   }
 }
