96ae1d1e6ce65f5b93843481314c23196f0034ba
Jose A. Pinell <japinell@yahoo.com>
2025-09-28 20:16:25 -0700

    Added video property to Glyph model

--- lib/models/glyph.dart
+++ lib/models/glyph.dart
@@ -2,19 +2,26 @@ class Glyph {
   final String glyph;
   final String info;
   final String audio;
+  final String video;
 
-  const Glyph({required this.glyph, required this.info, required this.audio});
+  const Glyph({
+    required this.glyph,
+    required this.info,
+    required this.audio,
+    required this.video,
+  });
 
   factory Glyph.fromJson(Map<String, dynamic> json) {
     return Glyph(
       glyph: json["glyph"]?.toString() ?? "",
       info: json["info"]?.toString() ?? "",
       audio: json["audio"]?.toString() ?? "",
+      video: json["video"]?.toString() ?? "",
     );
   }
 
   Map<String, dynamic> toJson() {
-    return {"glyph": glyph, "info": info, "audio": audio};
+    return {"glyph": glyph, "info": info, "audio": audio, "video": video};
   }
 
   @override
@@ -23,14 +30,16 @@ class Glyph {
     return other is Glyph &&
         other.glyph == glyph &&
         other.info == info &&
-        other.audio == audio;
+        other.audio == audio &&
+        other.video == video;
   }
 
   @override
-  int get hashCode => glyph.hashCode ^ info.hashCode ^ audio.hashCode;
+  int get hashCode =>
+      glyph.hashCode ^ info.hashCode ^ audio.hashCode ^ video.hashCode;
 
   @override
   String toString() {
-    return "Glyph(glyph: $glyph, info: $info, audio: $audio)";
+    return "Glyph(glyph: $glyph, info: $info, audio: $audio, video: $video)";
   }
 }
